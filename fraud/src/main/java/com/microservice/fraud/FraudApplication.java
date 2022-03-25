0f92c5ea86676f44d6f4a8316f3cea87623cbfef
Pavan <pavankumar1597@gmail.com>
2022-03-26 00:57:01 +0530

    amigoes fraud customer microservices

--- fraud/src/main/java/com/microservice/fraud/FraudApplication.java
+++ fraud/src/main/java/com/microservice/fraud/FraudApplication.java
@@ -2,9 +2,11 @@ package com.microservice.fraud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FraudApplication {
 
     public static void main(String[] args) {
